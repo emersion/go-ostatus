e8f77a4dc23a5ae0202e549f61586641fd26dc65
emersion <contact@emersion.fr>
2017-04-17 12:45:47 +0200

    ostatus: adds links rel

--- ostatus.go
+++ ostatus.go
@@ -1,2 +1,11 @@
 // Package ostatus implements the OStatus protocol suite.
 package ostatus
+
+// OStatus links.
+const (
+	LinkProfilePage = "http://webfinger.net/rel/profile-page"
+	LinkUpdatesFrom = "http://schemas.google.com/g/2010#updates-from"
+	LinkSalmon = "salmon"
+	LinkMagicPublicKey = "magic-public-key"
+	LinkSubscribe = "http://ostatus.org/schema/1.0/subscribe"
+)
